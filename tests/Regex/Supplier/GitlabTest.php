0232c67019e11c4bee4ee9bfec9575b67e0854e5
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2024-11-26 10:24:19 +0100

    Fix style issues

--- tests/Regex/Supplier/GitlabTest.php
+++ tests/Regex/Supplier/GitlabTest.php
@@ -7,23 +7,23 @@ use PHPUnit\Framework\TestCase;
 
 final class GitlabTest extends TestCase
 {
-	public function testDetectSecret(): void
-	{
-		$haystack = 'bar glpat-mBvGsDcJUvxvFZktWpzz baz';
-		$detector = Detector::create()->useSuppliers(new Gitlab());
-		$result   = $detector->detectIn($haystack);
+    public function testDetectSecret(): void
+    {
+        $haystack = 'bar glpat-mBvGsDcJUvxvFZktWpzz baz';
+        $detector = Detector::create()->useSuppliers(new Gitlab());
+        $result   = $detector->detectIn($haystack);
 
-		$this->assertTrue($result->wasSecretDetected());
-		$this->assertCount(1, $result->matches());
-	}
+        $this->assertTrue($result->wasSecretDetected());
+        $this->assertCount(1, $result->matches());
+    }
 
-	public function testDontDetectSecret(): void
-	{
-		$haystack = 'bar glpat-mBvGsDcJUvx... gitlab glpat-15487234 glpat-mBvG{}_JUvxvFZktWpzz';
-		$detector = Detector::create()->useSuppliers(new Gitlab());
-		$result   = $detector->detectIn($haystack);
+    public function testDontDetectSecret(): void
+    {
+        $haystack = 'bar glpat-mBvGsDcJUvx... gitlab glpat-15487234 glpat-mBvG{}_JUvxvFZktWpzz';
+        $detector = Detector::create()->useSuppliers(new Gitlab());
+        $result   = $detector->detectIn($haystack);
 
-		$this->assertFalse($result->wasSecretDetected());
-		$this->assertCount(0, $result->matches());
-	}
+        $this->assertFalse($result->wasSecretDetected());
+        $this->assertCount(0, $result->matches());
+    }
 }
